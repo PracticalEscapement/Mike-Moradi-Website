1cf1b88439be60497478031438f3a7ba93eeb299
Michael Moradi <69914892+PracticalEscapement@users.noreply.github.com>
2022-05-24 17:41:52 -0400

    Navigation (#1)
    * Basic navigation system
    
    * addition of page5

---
+++ src/context/TabNavigationReducer.js
@@ -0,0 +1,56 @@
+const TabNavigationReducer = (state, action) => {
+  switch (action.type) {
+    case 'PAGE1':
+      return {
+        pages: {
+          page1: false,
+          page2: true,
+          page3: true,
+          page4: true,
+          page5: true,
+        }
+      }
+    case 'PAGE2':
+      return {
+        pages: {
+          page1: false,
+          page2: false,
+          page3: true,
+          page4: true,
+          page5: true,
+        }
+      }
+    case 'PAGE3':
+      return {
+        pages: {
+          page1: false,
+          page2: false,
+          page3: false,
+          page4: true,
+          page5: true,
+        }
+      }
+    case 'PAGE4':
+      return {
+        pages: {
+          page1: false,
+          page2: false,
+          page3: false,
+          page4: false,
+          page5: true,
+        }
+      }
+    case 'PAGE5':
+      return {
+        pages: {
+          page1: false,
+          page2: false,
+          page3: false,
+          page4: false,
+          page5: false,
+        }
+      }
+  }
+}
+
+export default TabNavigationReducer
