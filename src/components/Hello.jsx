13a4d8480919b0daf18a5d15b41286df0f97391f
Michael Moradi <69914892+PracticalEscapement@users.noreply.github.com>
2022-06-06 14:44:56 -0400

    Responsive design (#3)
    * modifications to layout and first media queries
    
    * color changes
    
    * fix confusing state
    
    * media edit

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -5,11 +5,18 @@ import './Hello.css'
 function Hello() {
   const initialState = {
     pages: {
-      page1: false,
-      page2: true,
-      page3: true,
-      page4: true,
-      page5: true,
+      page1: true,
+      page2: false,
+      page3: false,
+      page4: false,
+      page5: false,
+    },
+    pageLabels: {
+      page1: true,
+      page2: false,
+      page3: false,
+      page4: false,
+      page5: false,
     }
   }
 
@@ -21,47 +28,67 @@ function Hello() {
   const pageFour = state.pages.page4
   const pageFive = state.pages.page5
 
+  const pageOneLabel = state.pageLabels.page1
+  const pageTwoLabel = state.pageLabels.page2
+  const pageThreeLabel = state.pageLabels.page3
+  const pageFourLabel = state.pageLabels.page4
+  const pageFiveLabel = state.pageLabels.page5
+
+  const handlePageChange = (page) => {
+    dispatch({type: page})
+  }
+
   return (
     <>
       <div className='page-container'>
         <div
-          className={!pageOne ? 'page-1' : 'page-1 page-active'}
-          onClick={() => dispatch({ type: 'PAGE1'})}
+          className={pageOne ? 'page-1 page-active' : 'page-1'}
+          onClick={() => handlePageChange('PAGE1')}
         >
-          <div className='page-label'>
-            <h3 className='page-label-text page-label-text-dark'>Hello</h3>
+          <div className={pageOneLabel ? 'page-label-active' : 'page-label'}>
+            <h3 className={pageOneLabel ? 'page-label-text-active page-label-text-dark' : 'page-label-text page-label-text-dark'}>
+              Hello
+            </h3>
           </div>
         </div>
         <div
-          className={!pageTwo ? 'page-2' : 'page-2 page-active'}
-          onClick={() => dispatch({ type: 'PAGE2'})}
+          className={pageTwo ? 'page-2 page-active' : 'page-2'}
+          onClick={() => handlePageChange('PAGE2')}
         >
-          <div className='page-label'>
-            <h3 className='page-label-text page-label-text-light'>Projects</h3>
+          <div className={pageTwoLabel ? 'page-label-active' : 'page-label'}>
+            <h3 className={pageTwoLabel ? 'page-label-text-active page-label-text-light' : 'page-label-text page-label-text-light'}>
+              Projects
+            </h3>
           </div>
         </div>
         <div
-          className={!pageThree ? 'page-3' : 'page-3 page-active'}
-          onClick={() => dispatch({ type: 'PAGE3'})}
+          className={pageThree ? 'page-3 page-active' : 'page-3'}
+          onClick={() => handlePageChange('PAGE3')}
         >
-          <div className='page-label'>
-            <h3 className='page-label-text page-label-text-dark'>Resume</h3>
+          <div className={pageThreeLabel ? 'page-label-active' : 'page-label'}>
+            <h3 className={pageThreeLabel ? 'page-label-text-active page-label-text-light' : 'page-label-text page-label-text-light'}>
+              Resume
+            </h3>
           </div>
         </div>
         <div
-          className={!pageFour ? 'page-4' : 'page-4 page-active'}
-          onClick={() => dispatch({ type: 'PAGE4'})}
+          className={pageFour ? 'page-4 page-active' : 'page-4'}
+          onClick={() => handlePageChange('PAGE4')}
         > 
-          <div className='page-label'>
-            <h3 className='page-label-text page-label-text-light'>Contact</h3>
+          <div className={pageFourLabel ? 'page-label-active' : 'page-label'}>
+            <h3 className={pageFourLabel ? 'page-label-text-active page-label-text-light' : 'page-label-text page-label-text-light'}>
+              Contact
+            </h3>
           </div>
         </div>
         <div
-          className={!pageFive ? 'page-5' : 'page-5 page-active'}
-          onClick={() => dispatch({ type: 'PAGE5'})}
+          className={pageFive ? 'page-5 page-active' : 'page-5'}
+          onClick={() => handlePageChange('PAGE5')}
         > 
-          <div className='page-label'>
-            <h3 className='page-label-text page-label-text-dark'>About</h3>
+          <div className={pageFiveLabel ? 'page-label-active' : 'page-label'}>
+            <h3 className={pageFiveLabel ? 'page-label-text-active page-label-text-dark' : 'page-label-text page-label-text-dark'}>
+              About
+            </h3>
           </div>
         </div>
         
