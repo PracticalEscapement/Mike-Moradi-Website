2f77e62b4646aa3a78985e3b994599050f81fdfb
Michael Moradi <themoradis1@gmail.com>
2022-06-07 22:20:39 -0400

    create a separate route for each tab on UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import Hello from './components/Hello'
+import NotFound from './components/pages/NotFound'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { TabNavigationProvider } from './context/TabNavigationContext.js'
 
 function App() {
   return (
     <>
       <TabNavigationProvider>
-        <Hello />
+        <Router>
+          <div>
+            <Routes>
+              <Route path='/' element={<Hello pageRoute='PAGE1' />} />
+              <Route path='/projects' element={<Hello pageRoute='PAGE2'  />} />
+              <Route path='/resume' element={<Hello pageRoute='PAGE3' />} />
+              <Route path='/contact' element={<Hello pageRoute='PAGE4' />} />
+              <Route path='/about' element={<Hello pageRoute='PAGE5' />} />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </TabNavigationProvider>
     </>
   );
