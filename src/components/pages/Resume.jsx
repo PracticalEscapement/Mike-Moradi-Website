82e2218adc9d80c76f4955c1f2624a19ab04b7f0
Michael Moradi <themoradis1@gmail.com>
2022-07-26 14:41:36 -0400

    view resume button v1

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import Card from '../card/Card'
+import myResume from '../../assests/resume-image.png'
+import './resume.css'
 
 function Resume() {
   return (
     <>
-      <Card text={'Resume Page'} />
+      <div className='resume-container'>
+        <div className='resume-pdf__container'>
+          <div className='resume-pdf__img--container'>
+            <img className='resume-pdf__img' src={myResume} alt='Resume-img'/>
+          </div>
+            <h2 className='resume-pdf__heading'>Click To<br/>View PDF</h2>
+        </div>
+      </div>
     </>
   )
 }
